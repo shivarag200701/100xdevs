8293b4e57541ca6a5a9fe012cec53d6fa429276f
Shiva Raghav <shivaraghav200701@gmail.com>
2025-09-23 23:33:58 -0500

    useMemo and useCakkback assignment

---
+++ week_6/use-memo/src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import { Assignment1 } from "./Components/Assignment1";
+import { Assignment2 } from "./Components/Assignment2";
+import { Assignment3 } from "./Components/Assignment3";
+
+function App() {
+  return (
+    <>
+      {/* <Assignment1 /> */}
+      {/* <Assignment2 /> */}
+      <Assignment3 />
+    </>
+  );
+}
+
+export default App;
---
+++ week_6/use-memo/src/Components/Assignment1.jsx
@@ -0,0 +1,37 @@
+import { useState, useMemo } from "react";
+
+// In this assignment, your task is to create a component that performs an expensive calculation (finding the factorial) based on a user input.
+// Use useMemo to ensure that the calculation is only recomputed when the input changes, not on every render.
+
+export function Assignment1() {
+  const [input, setInput] = useState(0);
+  // Your solution starts here
+  const expensiveValue = useMemo(
+    function () {
+      if (input < 0) {
+        return "Factorial is not defined for negative numbers.";
+      } else if (input === 0 || input === 1) {
+        return 1;
+      } else {
+        let result = 1;
+        for (let i = 2; i <= input; i++) {
+          result *= i;
+        }
+        return result;
+      }
+    },
+    [input]
+  );
+  // Your solution ends here
+
+  return (
+    <div>
+      <input
+        type="number"
+        value={input}
+        onChange={(e) => setInput(Number(e.target.value))}
+      />
+      <p>Calculated Value: {expensiveValue}</p>
+    </div>
+  );
+}
