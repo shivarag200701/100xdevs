ff5e21b36d85f69f49f6674180984bfdaabc8b53
Shiva Raghav <shivaraghav200701@gmail.com>
2025-09-26 14:08:33 -0500

    recoil optimized

--- state-management/src/App.jsx
+++ state-management/src/App.jsx
@@ -1,6 +1,12 @@
 import "./App.css";
-import { RecoilRoot, useRecoilState, useRecoilValue } from "recoil";
+import {
+  RecoilRoot,
+  useRecoilState,
+  useRecoilValue,
+  useSetRecoilState,
+} from "recoil";
 import { countAtom } from "./store/atoms/count";
+import { useMemo } from "react";
 
 function App() {
   return (
@@ -13,10 +19,13 @@ function App() {
 }
 
 function Count() {
+  console.log("redenered");
+
   return (
     <div>
       <CountRenderer />
       <Buttons />
+      <Iseven />
     </div>
   );
 }
@@ -25,25 +34,35 @@ function CountRenderer() {
   return <div>{count}</div>;
 }
 function Buttons() {
-  const [count, setCount] = useRecoilState(countAtom);
+  console.log("buttomn rendered");
+
+  const setCount = useSetRecoilState(countAtom);
   return (
     <div>
       <button
         onClick={() => {
-          setCount(count + 1);
+          setCount((prev) => prev + 1);
         }}
       >
         Increase
       </button>
       <button
         onClick={() => {
-          setCount(count - 1);
+          setCount((prev) => prev - 1);
         }}
       >
         Decrease
       </button>
     </div>
   );
+}
+function Iseven() {
+  const count = useRecoilValue(countAtom);
+  const isEven = useMemo(() => {
+    return count % 2 == 0;
+  }, [count]);
+
+  return <>{isEven ? "It is even" : ""}</>;
 }
 
 export default App;
