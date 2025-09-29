7d7944168a9532bdf7a62ad3f3a4f58f194ddf6f
Shiva Raghav <shivaraghav200701@gmail.com>
2025-09-29 13:29:46 -0500

    atom family testing

--- atom-family/src/App.jsx
+++ atom-family/src/App.jsx
@@ -1,17 +1,32 @@
 import "./App.css";
-import { useRecoilValue, RecoilRoot } from "recoil";
+import { useRecoilValue, RecoilRoot, useSetRecoilState } from "recoil";
 import { todosAtomFamily } from "./atom";
+import { useEffect } from "react";
 
 function App() {
   return (
     <>
       <RecoilRoot>
+        <AtomStateUpdate />
         <Todo id={2} />
         <Todo id={1} />
         <Todo id={2} />
       </RecoilRoot>
     </>
   );
+}
+
+function AtomStateUpdate() {
+  const updateTodo2 = useSetRecoilState(todosAtomFamily(2));
+  useEffect(() => {
+    setTimeout(() => {
+      updateTodo2({
+        id: 2,
+        title: "New todo",
+        description: "I added a new todo",
+      });
+    }, 5000);
+  }, []);
 }
 
 function Todo({ id }) {
