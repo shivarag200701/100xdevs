007d86fecf4db000ae8be7c3dfcd3e5703d4a0c7
Shiva Raghav <shivaraghav200701@gmail.com>
2025-10-15 11:54:00 -0500

    Maps using generics

--- express-mongodb-prisma-react-recoil/week12/dist/index.js
+++ express-mongodb-prisma-react-recoil/week12/dist/index.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const user = {
-    bd456: { id: "bd456", name: "shiva", age: 24 },
-};
+const users = new Map();
+users.set("b4dsa", { name: "shiva", age: 23, email: "sadadsad" });
+users.set("b4d", { name: "sragh", age: 22, email: "sadad" });
+users.get("b4d");
--- express-mongodb-prisma-react-recoil/week12/src/index.ts
+++ express-mongodb-prisma-react-recoil/week12/src/index.ts
@@ -1,11 +1,12 @@
-interface User {
-  id: string;
+type User = {
   name: string;
   age: number;
-}
-
-type Users = Record<string, User>;
-
-const user: Users = {
-  bd456: { id: "bd456", name: "shiva", age: 24 },
+  email: string;
 };
+
+const users = new Map<string, User>();
+
+users.set("b4dsa", { name: "shiva", age: 23, email: "sadadsad" });
+users.set("b4d", { name: "sragh", age: 22, email: "sadad" });
+
+users.get("b4d");
