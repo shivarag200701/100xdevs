020269064d37276269686e9466531a25d8dc4414
Shiva Raghav <shivaraghav200701@gmail.com>
2025-10-22 14:03:30 -0500

    api troutes

--- nextjs/app/page.tsx
+++ nextjs/app/page.tsx
@@ -3,25 +3,11 @@ import axios from "axios";
 
 async function fetchData() {
   await new Promise((r) => setTimeout(r, 5000));
-  const res = await axios.get(
-    "https://blogging_website.shivaraghav200701.workers.dev/api/v1/blog/bulk",
-    {
-      headers: {
-        Authorization:
-          "bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjZjZjg0ZGIwLWFmMTktNDcxMC04M2FiLTc1OTkyNDU1MTVhMyJ9.CMqn9FheUOCD31FTa7gqQMH3_HVUmjv9Q_TLcJV6HUc",
-      },
-    }
-  );
+  const res = await axios.get("http://localhost:3001/api/user");
   return res.data;
 }
 
 export default async function Home() {
-  const blogs = await fetchData();
-  return (
-    <div>
-      {blogs.map((blog) => (
-        <h1 key={blog.id}>{blog.content}</h1>
-      ))}
-    </div>
-  );
+  const user = await fetchData();
+  return <div>{user.name}</div>;
 }
